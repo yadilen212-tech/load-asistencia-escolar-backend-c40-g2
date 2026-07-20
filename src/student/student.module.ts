98100dbe2d0ffe2f88f869dc67e224109ee754f4
yura-pruebas <leninner@gmail.com>
2026-07-20 18:32:35 -0500

    Update StudentModule to import PrismaModule

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { StudentController } from './student.controller'
 import { StudentService } from './student.service'
+import { PrismaModule } from '../prisma/prisma.module'
 
 @Module({
+  imports: [PrismaModule],
   controllers: [StudentController],
   providers: [StudentService],
 })
